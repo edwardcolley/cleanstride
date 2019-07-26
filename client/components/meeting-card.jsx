c14a1142a9bfdb23262d158f7774126acf8a2912
edwardcolley <edward.colley@outlook.com>
2019-07-26 22:44:11 +0000

    meeting list backend

--- client/components/meeting-card.jsx
+++ client/components/meeting-card.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Row, Col, Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Row, Col, Card, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 function Meetingcard(props) {
   return (
     <React.Fragment>
       <Row className="mt-5">
-        <Col>
-          <Card>
+        <Col md={{ size: 6, offset: 3 }}>
+          <Card className="shadow" body inverse style={{ backgroundColor: '#A9A9A9', borderColor: '#A9A9A9' }}>
             <CardBody>
               <CardTitle>{props.input.day}</CardTitle>
-              <CardSubtitle>{props.input.city}</CardSubtitle>
-              <CardSubtitle>{props.input.type}</CardSubtitle>
+              <CardSubtitle >{props.input.city}</CardSubtitle>
+              <CardSubtitle className="mt-1">{props.input.type}</CardSubtitle>
               <CardSubtitle>{props.input.name}</CardSubtitle>
-            </CardBody>
-            <CardBody>
-              <CardText>{props.input.name}</CardText>
+              <CardSubtitle className="mt-1">{props.input.time}</CardSubtitle>
+              <CardText className="mt-2">{props.input.address}</CardText>
               <CardSubtitle>{props.input.zip}</CardSubtitle>
+              <Button className="mt-2" color="primary">Favorite</Button>{' '}
             </CardBody>
           </Card>
         </Col>
