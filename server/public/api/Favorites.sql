607fcacbf50e0b5a44ea8c24aac2e7854841e4a6
edwardcolley <edward.colley@outlook.com>
2019-08-01 23:40:50 +0000

    added new sql dumps

--- server/public/api/Favorites.sql
+++ server/public/api/Favorites.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 30, 2019 at 07:23 PM
+-- Generation Time: Aug 01, 2019 at 11:40 PM
 -- Server version: 5.7.26-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.1
 
@@ -28,7 +28,6 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Favorites` (
   `id` mediumint(8) UNSIGNED NOT NULL,
-  `program` enum('AA','Al-Anon','NA','OA') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `program_id` mediumint(8) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -36,39 +35,12 @@ CREATE TABLE `Favorites` (
 -- Dumping data for table `Favorites`
 --
 
-INSERT INTO `Favorites` (`id`, `program`, `program_id`) VALUES
-(55, 'AA', 4),
-(56, 'AA', 3),
-(57, 'AA', 3),
-(58, 'AA', 6),
-(59, 'AA', 1),
-(60, 'AA', 1),
-(61, 'AA', 3),
-(62, 'AA', 3),
-(63, 'AA', 3),
-(64, 'AA', 1),
-(65, 'AA', 1),
-(66, 'AA', 1),
-(67, 'AA', 1),
-(68, 'AA', 1),
-(69, 'AA', 1),
-(70, 'AA', 1),
-(71, 'AA', 1),
-(72, 'AA', 3),
-(73, 'AA', 4),
-(74, 'AA', 56),
-(75, 'AA', 57),
-(76, 'AA', 58),
-(77, 'AA', 58),
-(78, 'AA', 58),
-(79, 'AA', 58),
-(80, 'AA', 3),
-(81, 'AA', 3),
-(82, 'AA', 3),
-(83, 'AA', 3),
-(84, 'AA', 4),
-(85, 'AA', 5),
-(86, 'AA', 5);
+INSERT INTO `Favorites` (`id`, `program_id`) VALUES
+(411, 62),
+(412, 1),
+(413, 2),
+(414, 33),
+(415, 46);
 
 --
 -- Indexes for dumped tables
@@ -88,7 +60,7 @@ ALTER TABLE `Favorites`
 -- AUTO_INCREMENT for table `Favorites`
 --
 ALTER TABLE `Favorites`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=416;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
