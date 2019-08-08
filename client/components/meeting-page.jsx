368eb0ba00d14d76e5e5174f70a79fc7710b8f9f
edwardcolley <edward.colley@outlook.com>
2019-08-08 21:34:58 +0000

    added the NA and OA meeting schedule

--- client/components/meeting-page.jsx
+++ client/components/meeting-page.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import { Container, Row, Col, Form, FormGroup, Input, Button, Card, CardText, InputGroup, CardTitle } from 'reactstrap';
+import { Container, Row, Col, Form, FormGroup, Input, Button, Card, InputGroup, CardTitle } from 'reactstrap';
 import Meetingcard from './meeting-card';
 import NavBar from './nav-bar';
 import { Link } from 'react-router-dom';
@@ -161,24 +161,23 @@ export default class Meetings extends React.Component {
         <Row className="mt-2">
           <Col md={{ size: 6, offset: 3 }}>
             <Card body inverse color="info">
-              <CardTitle className="text-center">Alcoholics Anonymous</CardTitle>
-              <CardText>
-                <ul>
-                  <li>C = Closed, alcholics only</li>
-                  <li>O = Open</li>
-                  <li>Y = Young People</li>
-                  <li>~ = Spanish speaking</li>
-                  <li>W = Women's</li>
-                  <li>M = Men's</li>
-                  <li>GA = Gay</li>
-                  <li>CC = Childcare Available</li>
-                  <li>BG = Beginner</li>
-                  <li>TA = Meditation</li>
-                  <li>SP = Speaker</li>
-                  <li>SE = Secular</li>
-                  <li>TG = Transgender</li>
-                </ul>
-              </CardText>
+              <CardTitle className="text-center"><h5>Alcoholics Anonymous</h5></CardTitle>
+              <h6 className="text-center">Orange County AA: <br/> (714) 556-4555</h6>
+              <ul>
+                <li><span className="font-weight-bold">C</span> = Closed, alcholics only</li>
+                <li><span className="font-weight-bold">O</span> = Open</li>
+                <li><span className="font-weight-bold">Y</span> = Young People</li>
+                <li><span className="font-weight-bold">~</span> = Spanish speaking</li>
+                <li><span className="font-weight-bold">W</span> = Women's</li>
+                <li><span className="font-weight-bold">M</span> = Men's</li>
+                <li><span className="font-weight-bold">GA</span> = Gay</li>
+                <li><span className="font-weight-bold">CC</span> = Childcare Available</li>
+                <li><span className="font-weight-bold">BG</span> = Beginner</li>
+                <li><span className="font-weight-bold">TA</span> = Meditation</li>
+                <li><span className="font-weight-bold">SP</span> = Speaker</li>
+                <li><span className="font-weight-bold">SE</span> = Secular</li>
+                <li><span className="font-weight-bold">TG</span> = Transgender</li>
+              </ul>
             </Card>
           </Col>
         </Row>
@@ -187,16 +186,61 @@ export default class Meetings extends React.Component {
         <Row className="mt-2">
           <Col md={{ size: 6, offset: 3 }}>
             <Card body inverse color="danger" className="shadow">
-              <CardTitle className="text-center">Al-Anon</CardTitle>
-              <CardText>
-                <ul>
-                  <li>O = Open Meetings - Visitors are welcome to attend, but MAY NOT PARTICIPATE</li>
-                  <li>W = Wheelchair Accessible</li>
-                  <li>B = Beginner's meeting</li>
-                  <li>A = Al-Anon and Alateen meetings at the same time and location</li>
-                  <li>Y = Al-Anon and Young Alateen meetings at the same time and location</li>
-                </ul>
-              </CardText>
+              <CardTitle className="text-center"><h5>Al-Anon</h5></CardTitle>
+              <h6 className="text-center">Al-Anon Family Groups (toll-free):<br/> (888) 425-2666</h6>
+              <ul>
+                <li><span className="font-weight-bold">O</span> = Open Meetings - Visitors are welcome to attend, but MAY NOT PARTICIPATE</li>
+                <li><span className="font-weight-bold">W</span> = Wheelchair Accessible</li>
+                <li><span className="font-weight-bold">B</span> = Beginner's meeting</li>
+                <li><span className="font-weight-bold">A</span> = Al-Anon and Alateen meetings at the same time and location</li>
+                <li><span className="font-weight-bold">Y</span> = Al-Anon and Young Alateen meetings at the same time and location</li>
+              </ul>
+            </Card>
+          </Col>
+        </Row>
+          }
+          {this.state.program === 'NA' &&
+        <Row className="mt-2">
+          <Col md={{ size: 6, offset: 3 }}>
+            <Card body inverse className="shadow" style={{ backgroundColor: '#800080', borderColor: '#800080' }}>
+              <CardTitle className="text-center"><h5>Narcotics Anonymous</h5></CardTitle>
+              <h6 className="text-center">24-Hour Helpline: <br /> (714) 590-2388</h6>
+              <ul>
+                <li><span className="font-weight-bold">7D</span> = Seven days per week.</li>
+                <li><span className="font-weight-bold">BK</span> = Book Study - Approved NA literature.</li>
+                <li><span className="font-weight-bold">C</span> = Closed</li>
+                <li><span className="font-weight-bold">CC</span> = Child Care</li>
+                <li><span className="font-weight-bold">CN</span> = Candlelight</li>
+                <li><span className="font-weight-bold">D</span> = Discussion - This meeting invites participation by all attendees.</li>
+                <li><span className="font-weight-bold">LT</span> = Literature Study</li>
+                <li><span className="font-weight-bold">M</span> = Men Only</li>
+                <li><span className="font-weight-bold">NC</span> = No Children</li>
+                <li><span className="font-weight-bold">NS</span> = Non-Smoking - Smoking is not allowed at this meeting.</li>
+                <li><span className="font-weight-bold">O</span> = Open - This meeting is open to addicts and non-addicts alike. All are welcome</li>
+                <li><span className="font-weight-bold">QA</span> = Question/Answer</li>
+                <li><span className="font-weight-bold">OUT</span> = Meeting Held Outside</li>
+                <li><span className="font-weight-bold">S</span> = Smoking</li>
+                <li><span className="font-weight-bold">SS</span> = Step Study</li>
+                <li><span className="font-weight-bold">SP</span> = Spanish</li>
+                <li><span className="font-weight-bold">W</span> = Women Only</li>
+                <li><span className="font-weight-bold">Y</span> = Youth</li>
+              </ul>
+            </Card>
+          </Col>
+        </Row>
+          }
+          {this.state.program === 'OA' &&
+        <Row className="mt-2">
+          <Col md={{ size: 6, offset: 3 }}>
+            <Card body inverse className="shadow" color="success">
+              <CardTitle className="text-center"><h5>Overeaters Anonymous</h5></CardTitle>
+              <h6 className="text-center">Call for more information: <br /> (505) 891-4320</h6>
+              <ul>
+                <li><span className="font-weight-bold">Open Meeting:</span> Open to OA members and non-OA visitors. All meetings are considered open unless otherwise indicated.</li>
+                <li><span className="font-weight-bold">Closed Meeting:</span> Open to anyone with a desire to stop eating compulsively or anyone who thinks they may have a problem with compulsive overeating.</li>
+                <li><span className="font-weight-bold">Special Focus Meetings:</span> (LGBT, Women's, 100-pounders, maintainers, anorexic/bulimic, etc) are composed of individuals who feel they can more readily identify with fellow OAers with similar attributes.</li>
+                <li><span className="font-weight-bold">Special Topic Meetings:</span> (Big Book Study, Lifeline, Pseaker, Newcomer, etc.) have a defined topic of discussion.</li>
+              </ul>
             </Card>
           </Col>
         </Row>
